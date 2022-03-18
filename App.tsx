b1b9fe1247f2a4571696b216936ca50af13d748d
Kenneth Lewenhagen <lewenhagen@live.com>
2022-03-18 15:22:33 +0100

    01 done

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import warehouse from './assets/warehouse.jpg';
+import dna from './assets/img/dna.png';
 import Stock from './components/Stock.tsx';
 
 export default function App() {
   return (
-  <SafeAreaView style={styles.container}>
+  <SafeAreaView style={styles.safe}>
     <View style={styles.container}>
-      <Text style={{color: '#33c', fontSize: 42}}>Lager-Appen</Text>
-      <Image source={warehouse} style={{ width: 320, height: 240 }} />
+      <Text style={styles.title}>DNA Lagret</Text>
+      <Image source={dna} style={styles.logo} />
       <Stock />
       <StatusBar style="auto" />
     </View>
@@ -20,6 +20,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eeede8',
+    color: "#084C61",
+    fontFamily: 'Arial',
   },
+  safe: {
+    backgroundColor: "#084C61",
+    flex: 1
+  },
+  title: {
+      fontSize: 54,
+      textAlign: "center",
+      color: "#084C61",
+  },
+  logo: {
+    borderRadius: 100,
+    width: 240,
+    height: 240,
+    alignSelf: "center"
+  }
 });
--- components/Stock.tsx
+++ components/Stock.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
 import config from "../config/config.json";
 
@@ -11,19 +11,36 @@ function StockList() {
       .then(result => setProducts(result.data));
   }, []);
 
- const list = products.map((product, index) => <Text key={index}>{ product.name }</Text>);
+ const list = products.map((product, index) => <Text style={styles.list} key={index}>{ product.name.replaceAll('"', '') } ({ product.stock })</Text>);
 
   return (
-    <View>
-      {list}
+    <View style={styles.list}>
+      { list }
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  stock: {
+    fontSize: 30,
+    color: "#084C61",
+    textAlign: "center",
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+
+
+  },
+  list: {
+    textAlign: "center",
+    color: "#000",
+    fontSize: 20,
+  }
+});
+
 export default function Stock() {
     return (
         <View>
-            <Text style={{ color: '#333', fontSize: 24 }}>Lagerförteckning</Text>
+            <Text style={styles.stock}>Lagerstatus</Text>
             <StockList />
         </View>
     )
